b5017ef63d9c9303e2711e99f9ad106f1144b1e6
rahul-wakle <rahul.wakle@celoxis.com>
2025-09-07 15:45:54 +0530

    fixed

---
+++ practice/HelloWorld.js
@@ -0,0 +1,16 @@
+console.log("hello node js with 981")
+
+
+
+function Add(a,b){
+    return a+b;
+
+}
+
+function Sub(a,b){
+    return b-a;
+    
+}
+
+
+module.exports = {Add, Sub}
---
+++ practice/Math.js
@@ -0,0 +1,5 @@
+
+const math = require('./HelloWorld')
+
+console.log(math.Add(2,4))
+console.log(math.Sub(4,7))
